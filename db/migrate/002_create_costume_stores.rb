d535d51b2781b5417a1b86a8735c42a7aeef294d
Iskra B <ibatistapoblete@gmail.com>
2019-07-11 04:19:21 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees_count
-      t.boolean :in_business
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.string :opening_time
       t.string :closing_time
       t.timestamps
